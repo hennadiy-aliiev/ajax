2b46ceaad16ae42aa5dc5c8bf3bd438c73d69073
hennadiy1aliiev <aliev.gennadiy@gmail.com>
2016-06-04 12:32:26 +0000

    add new file

---
+++ script.js
@@ -0,0 +1,18 @@
+$('#btn-send').click(function(e) {
+  // Please set your email in url field
+  $.ajax({
+      url: "https://formspree.io/YOUR EMAIL HERE",  
+      method: "POST",
+      error: function () {
+        $('#container').html('<h1>Error!</h1>')
+      },
+      data: {
+             email: email.value,
+             message: msg.value
+             },
+      dataType: "json"
+  }).done(function() {
+     $('#container').html('<h1>Thank you!</h1>')
+  } );
+  e.preventDefault();
+});
